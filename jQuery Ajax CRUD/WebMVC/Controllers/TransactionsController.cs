de048820cc0c07db00c1ebc35b7782f32cf328b5
nguyentuananh921 <nguyentuananh921@gmail.com>
2021-09-28 00:08:32 +0700

    Initial commit

---
+++ jQuery Ajax CRUD/WebMVC/Controllers/TransactionsController.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebMVC.Data;
+using WebMVC.Models;
+
+namespace WebMVC.Controllers
+{
+    public class TransactionsController : Controller
+    {
+        private readonly TransactionDbContext _context;
+
+        public TransactionsController(TransactionDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Transactions
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Transactions.ToListAsync());
+        }
+
+        //// GET: Transactions/Details/5
+        //public async Task<IActionResult> Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var transactionModel = await _context.Transactions
+        //        .FirstOrDefaultAsync(m => m.TransactionId == id);
+        //    if (transactionModel == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View(transactionModel);
+        //}
+
+        // GET: Transactions/AddOrEdit
+        // GET: Transactions/AddOrEdit/5
+        public async Task<IActionResult> AddOrEdit( int id=0)
+        {
+            if (id == 0)
+            {
+                //Add new 
+                return View();
+                
+            }
+            else
+            {
+                //Update
+                var transactionModel = await _context.Transactions.FindAsync(id);
+                if (transactionModel == null)
+                {
+                    return NotFound();
+                }
+                return View(transactionModel);
+
+            }
+                
+        }
+
+        // POST: Transactions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TransactionId,AccountNumber,BeneficiaryName,BankName,SWIFTCode,Amount,Date")] TransactionModel transactionModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(transactionModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(transactionModel);
+        }
+
+        // GET: Transactions/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var transactionModel = await _context.Transactions.FindAsync(id);
+            if (transactionModel == null)
+            {
+                return NotFound();
+            }
+            return View(transactionModel);
+        }
+
+        // POST: Transactions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("TransactionId,AccountNumber,BeneficiaryName,BankName,SWIFTCode,Amount,Date")] TransactionModel transactionModel)
+        {
+            if (id != transactionModel.TransactionId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(transactionModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TransactionModelExists(transactionModel.TransactionId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(transactionModel);
+        }
+
+        // GET: Transactions/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var transactionModel = await _context.Transactions
+                .FirstOrDefaultAsync(m => m.TransactionId == id);
+            if (transactionModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(transactionModel);
+        }
+
+        // POST: Transactions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var transactionModel = await _context.Transactions.FindAsync(id);
+            _context.Transactions.Remove(transactionModel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TransactionModelExists(int id)
+        {
+            return _context.Transactions.Any(e => e.TransactionId == id);
+        }
+    }
+}
---
+++ jQuery Ajax CRUD/WebMVC/Data/TransactionDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebMVC.Models;
+
+namespace WebMVC.Data
+{
+    public class TransactionDbContext : IdentityDbContext
+    {
+        public TransactionDbContext(DbContextOptions<TransactionDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<TransactionModel> Transactions { get; set; }
+    }
+}
